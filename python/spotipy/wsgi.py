47cb3ddfe8a0d0635df207e94308f9445c6f31c5
i001 <i001@i5.home.koko>
2025-11-08 22:26:28 +0100

    removed uneeded files

--- python/spotipy/wsgi.py
+++
@@ -1,5 +0,0 @@
-from gunicron_spotify_app import app  # Replace 'your_flask_app_file' with the name of your Python file without the .py extension
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3344, debug=True)
-
