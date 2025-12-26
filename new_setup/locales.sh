01dafd545d533233163305c044202b1857ec9714
ckokojambo <69429899+ckokojambo@users.noreply.github.com>
2025-12-26 20:36:58 +0100

    Add script to configure German locales and timezone

---
+++ new_setup/locales.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo locale-gen de_DE.UTF-8
+
+sudo update-locale \
+  LC_TIME=de_DE.UTF-8 \
+  LC_NUMERIC=de_DE.UTF-8 \
+  LC_MONETARY=de_DE.UTF-8 \
+  LC_MEASUREMENT=de_DE.UTF-8 \
+  LC_PAPER=de_DE.UTF-8 \
+  LC_NAME=de_DE.UTF-8 \
+  LC_ADDRESS=de_DE.UTF-8 \
+  LC_TELEPHONE=de_DE.UTF-8
+
+  sudo timedatectl set-timezone Europe/Berlin
+
+  
